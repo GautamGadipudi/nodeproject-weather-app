7f5dd29a2e1806c48cf62ed166cd9800af7fc519
Gautam Gadipudi <gautam.96.gadipudi@gmail.com>
2018-05-18 03:15:23 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode.js');
+
+const argv = yargs
+  .options({
+    address: {
+      demand: true,
+      alias: 'a',
+      describe: 'Address to fetch weather for.',
+      string: true
+    }
+  })
+  .help()
+  .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage)
+    console.log(errorMessage);
+  else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
+});
